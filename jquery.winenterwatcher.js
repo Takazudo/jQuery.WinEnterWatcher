2d815a6fb524bdafc65b6141e5a6f90ea5368dc9
Takeshi Takatsudo <takazudo@gmail.com>
2014-10-03 16:51:29 +0900

    initial commit

---
+++ jquery.winenterwatcher.js
@@ -0,0 +1,10 @@
+/*! jQuery.WinEnterWatcher (https://github.com/Takazudo/jQuery.WinEnterWatcher)
+ * lastupdate: 2014-10-03
+ * version: 0.1.0
+ * author: 'Takazudo' Takeshi Takatsudo <takazudo@gmail.com>
+ * License: MIT */
+(function() {
+
+
+
+}).call(this);
